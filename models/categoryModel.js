5a263de0622d49be8f769f4e140f131094b8ffc8
talib789 <taalibullah786@gmail.com>
2023-05-01 23:55:58 +0530

    first commit

---
+++ models/categoryModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema({
+  // Class
+  // _id: mongoose.Types.ObjectId,
+  name: String,
+});
+
+const categoryModel = mongoose.model(
+  "Categories",
+  categorySchema,
+  "categories"
+);
+
+module.exports = categoryModel;
